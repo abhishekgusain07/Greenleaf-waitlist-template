9130a05a884c9daccf3afe566e1926cb7a8c58ff
abhishekgusain07 <abhishek.gusain1007fb@gmail.com>
2025-04-19 23:35:33 +0530

    update: metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,13 +19,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark" suppressHydrationWarning>
-      <meta property="og:image" content="/opengraph-image.png" />
+      <meta property="og:image" content="/greenleaf.png" />
       <meta property="og:image:type" content="image/png" />
       <meta property="og:image:width" content="1280" />
       <meta property="og:image:height" content="832" />
       <meta
         property="og:site_name"
-        content="Next.js + Notion — Waitlist Template"
+        content="Greenleaf"
       />
       <meta
         property="og:url"
